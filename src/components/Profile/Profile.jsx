f43d05adae39c73eec5fb34b777e146e0577758b
Ivan Prodeus <learning.webpractice@gmail.com>
2022-10-13 22:06:47 +0300

    maked styles on profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,36 +1,44 @@
+import PropTypes from 'prop-types';
+import {UserCard, Avatar, Description, Name, Tag, Location, Stats, Label, Quantity, StatBox,} from './Profile.styled'
 
 
+export const Profile = ({ user }) => {
+  const { username, tag, location, avatar, stats } = user;
+  return (
+    <UserCard>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
 
-export const Profile = ({user}) => {
-  const {username, tag, location, avatar, stats} = user;
-    return(
+      <Stats>
+        <StatBox>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatBox>
+        <StatBox>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatBox>
+        <StatBox>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatBox>
+      </Stats>
+    </UserCard>
+  );
+};
 
-        <div className="profile">
-          <div className="description">
-            <img
-              src={avatar}
-              alt="User avatar"
-              className="avatar"
-            />
-            <p className="name">{username}</p>
-            <p className="tag">@{tag}</p>
-            <p className="location">{location}</p>
-          </div>
-            
-          <ul className="stats">
-            <li>
-              <span className="label">Followers</span>
-              <span className="quantity">{stats.followers}</span>
-            </li>
-            <li>
-              <span className="label">Views</span>
-              <span className="quantity">{stats.views}</span>
-            </li>
-            <li>
-              <span className="label">Likes</span>
-              <span className="quantity">{stats.likes}</span>
-            </li>
-          </ul>
-        </div>
-    )
-}
+UserCard.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
